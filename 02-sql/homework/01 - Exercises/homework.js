70443cd8723d8d3effbe62d76974679e5c8065c2
unknown <nacho.morales5mail.com>
2023-03-08 10:28:36 -0300

    Henry dia 36

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -4,12 +4,15 @@ const ejercicio02 = 'SELECT * FROM movies WHERE duration < 90;';
 
 const ejercicio03 = 'SELECT * FROM movies WHERE year >= 1930 AND year <= 1940;';
 
-const ejercicio04 = `'SELECT * FROM movies WHERE title LIKE ' % til %';`;
+const ejercicio03_2 = 'SELECT * FROM movies WHERE year BETWEEN 1930 AND 1940';
+
+const ejercicio04 = `'SELECT * FROM movies WHERE title LIKE '%til%';`;
 
 const ejercicio05 = 'SELECT * FROM movies WHERE ARRAY_LENGTH(actors, 1) = 1;';
+const ejercicio05_2 = 'SELECT * FROM movies WHERE CARDINALITY(actors) = 1;';
 
 const ejercicio06 =
-	'SELECT title, AVG(ratings_num) FROM movies, unnest(ratings) AS ratings_num GROUP BY title;';
+	'SELECT title, AVG(rating) FROM movies, unnest(ratings) AS rating GROUP BY title;';
 
 const ejercicio07 = `SELECT actors FROM movies WHERE title LIKE '%Fast and%' AND year = 2016;`;
 
